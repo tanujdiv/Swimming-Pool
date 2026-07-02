95973551d92795fa1233916da80918bbf699dd88
tanuj diwakar <tanujdiwakar999@gmail.com>
2026-07-02 16:21:44 +0530

    fix frontend layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Swimming Pool</title>
@@ -8,10 +9,50 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
+
 <body>
+
+    {{-- NAVBAR COMMON FOR ALL FRONTEND PAGES --}}
+    <nav class="navbar navbar-expand-lg navbar-dark custom-nav">
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="/">Aqua Pool</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="nav">
+                <ul class="navbar-nav ms-auto align-items-lg-center">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('booking') }}">Booking</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('memberships') }}">Membership</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('membership.renew.page') }}">
+                            Renew Membership
+                        </a>
+                    </li>
+                </ul>
+
+                <form action="/logout" method="POST" class="ms-lg-3 mt-3 mt-lg-0">
+                    @csrf
+                    <button class="logout-btn">Logout</button>
+                </form>
+            </div>
+        </div>
+    </nav>
 
     @yield('content')
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
